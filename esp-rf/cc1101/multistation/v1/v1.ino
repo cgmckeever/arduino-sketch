dc1799d74b8812bfd231986e287a924d4e85d602
cgmckeever <cgmckeever@rebelliondefense.com>
2022-06-07 18:58:49 -0500

    add in all libraries; minor wiring

--- esp-rf/cc1101/multistation/v1/v1.ino
+++ esp-rf/cc1101/multistation/v1/v1.ino
@@ -1,6 +1,17 @@
 #include <ConfigManager.h>
 ConfigManager configManager;
 
+#include <ArduinoLog.h>
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
+
+#include <rtl_433_ESP.h>
+rtl_433_ESP rf(-1);
+
+#include <RCSwitch.h>
+RCSwitch mySwitch = RCSwitch();
+
+// params
+//
 const char *settingsHTML = (char *)"/settings.html";
 const char *resetHTML = (char *)"/reset.html";
 const char *stylesCSS = (char *)"/styles.css";
@@ -17,21 +28,21 @@ struct Config {
 struct Metadata {
 } meta;
 
-template<typename T>
-void debug(T &msg, bool newline = false) {
-  DEBUG_MODE = true;
-  Serial.begin(115200);
-  DebugPrint(msg);
-  if (newline) DebugPrintln(F(""));
-  Serial.flush();
-  Serial.end();
-}
+
+#define JSON_MSG_BUFFER 512
+#define LOG_LEVEL LOG_LEVEL_VERBOSE
+
+// these can become CONFIGS
+#define CC1101_FREQUENCY 433.92
+#define RF_RECEIVER_GPIO 27
+#define RF_EMITTER_GPIO 26
+
+char messageBuffer[JSON_MSG_BUFFER];
 
 // ConfigManager
 //
 void configSetup() {
   DEBUG_MODE = true;
-  Serial.begin(115200);
 
   String sApName = "ESP";
 
@@ -61,8 +72,8 @@ void setConfigDefaults() {
 }
 
 void printConfig() {
-  debug("Configuration: ");
-  debug(config.deviceName, true);
+  Log.notice(F("Configuration" CR));
+  Log.notice(F("Device Name : %s" CR), config.deviceName);
 }
 
 void serveAssets(WebServer *server) {
@@ -112,14 +123,50 @@ void APICallback(WebServer *server) {
   printConfig();
 }
 
+// RTL
+//
+void rtlSetup() {
+    ELECHOUSE_cc1101.Init();
+    ELECHOUSE_cc1101.setMHZ(CC1101_FREQUENCY);
+    ELECHOUSE_cc1101.SetRx(CC1101_FREQUENCY);
+
+    Log.notice(F(" " CR));
+    Log.notice(F("****** RTL setup begin ******" CR));
+    rf.initReceiver(RF_RECEIVER_GPIO, CC1101_FREQUENCY);
+    rf.setCallback(rtl_433_Callback, messageBuffer, JSON_MSG_BUFFER);
+    rf.enableReceiver(RF_RECEIVER_GPIO);
+    Log.notice(F("****** RTL setup complete ******" CR));
+}
+
+void myswitchSetup() {
+    rf.disableReceiver();
+    mySwitch.enableTransmit(RF_EMITTER_GPIO);
+}
+
+void rtl_433_Callback(char* message) {
+    DynamicJsonDocument jsonBuffer(JSON_MSG_BUFFER);
+    deserializeJson(jsonBuffer, message);
+    logJson(jsonBuffer.as<JsonObject>());
+}
+
+void logJson(JsonObject jsondata)
+{
+    Log.notice(F("Received message : %s" CR), jsondata);
+}
+
 // Main
 //
 void setup() {
-  configSetup();
+    Serial.begin(115200);
+    Log.begin(LOG_LEVEL, &Serial);
+
+    configSetup();
+    rtlSetup();
 }
 
 void loop() {
-  configManager.loop();
+    configManager.loop();
+    rf.loop();
 }
 
 
